1f18e3dd2f1c32c28498b25a922a05d680dfdfbb
Hao SUN <xsunsmile@gmail.com>
2011-06-16 16:28:05 +0900

    add vpn_address

--- upload/tasks/puppet/manifests/nodes/inters.pp
+++ upload/tasks/puppet/manifests/nodes/inters.pp
@@ -11,7 +11,9 @@ node /__HOSTNAME_BASE__$/ {
 	tinc::vpn_net { inters:
 		tinc_interface => 'eth0',
 		tinc_internal_interface => 'eth0',
-		key_source_path => '/etc/tinc'
+		key_source_path => '/etc/tinc',
+		vpn_subnet_ip => $vpn_ipaddress,
+		vpn_subnet_mask => '255.255.255.0',
 	}
 }
 
